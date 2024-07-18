c56e9590629819e9b1309ef2ac8f7af25e35e630
Likith Aditya <140787525+thatsgkid@users.noreply.github.com>
2024-07-18 18:47:15 +0530

    Add files via upload

---
+++ ICT work/Chapter 9/activity9d.py
@@ -0,0 +1,11 @@
+"""LOOPS"""
+total = 0
+for x in range(1,11):
+    number1 = float(input("Enter a number: "))
+    total+=number1
+for x in range(1,11):
+    print(x)
+#No repeat until
+number = 0
+while number>=0:
+    number = float(input("Enter a negative value: "))
